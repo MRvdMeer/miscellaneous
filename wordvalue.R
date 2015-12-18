8c24911c06e1de7cc5f62395e270ef372c72893c
Maurits <mistamasta@gmail.com>
2015-12-18 09:51:01 +0100

    Create wordvalue.R

---
+++ wordvalue.R
@@ -0,0 +1,23 @@
+# compute letter values of each word
+# determine if the value is exactly 100
+# if so, save in an array and print
+
+compute.value <- function(word){
+  if(!is.character(word)){stop("not a word")}
+  if(length(word) > 1){stop("enter only one word")}
+  word.letters <- strsplit(tolower(word), split = "")[[1]]
+  sum(match(word.letters, letters), na.rm = FALSE)
+}
+
+word.list <- read.delim(file = "OpenTaal-210G-basis-gekeurd.txt", header = FALSE, stringsAsFactors = FALSE)[, 1]
+value.words <- NULL
+
+for(word in word.list){
+  x <- compute.value(word)
+  if(is.na(x)){next}
+  if(x == 100){
+    value.words <- c(value.words, word)
+  }
+}
+
+sort(tolower(value.words))
